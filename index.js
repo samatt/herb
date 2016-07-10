63fb2ca910c096c5b6c68ae548f9dcce8b418273
Surya Mattu <surya@suryamattu.com>
2016-07-10 14:17:50 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+let InterfaceUtils = require('../build/Release/herbivore')
+let Helper = require('../lib/herb-helper')
+let TransportSniffer = require('../lib/sniffer')
+
+module.exports.InterfaceUtils = InterfaceUtils
+module.exports.Helper = Helper
+module.exports.TransportSniffer = TransportSniffer
