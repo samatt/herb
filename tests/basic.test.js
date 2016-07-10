562196fec0df921a06cf59eb590985c8ccc5e12f
Surya Mattu <surya@suryamattu.com>
2016-07-10 14:17:45 -0400

    bunch of stuff

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -3,64 +3,49 @@ let herbivore = require('../build/Release/herbivore')
 let helper = require('../lib/herb-helper')
 let TransportSniffer = require('../lib/sniffer')
 
-describe('interface',() => {
+describe('interface', () => {
+  let Interface = herbivore.Interface
+  var iface = new Interface()
+  let ifaces = helper.parseSVec(iface.get_interfaces(false))
+  it('exists', () => {
+    expect(ifaces).to.have.length.above(0)
+  })
 
-    let Interface = herbivore.Interface
-    var interface = new Interface()
-    let ifaces = helper.parseSVec(interface.get_interfaces(false))
-    it('exists', () => {
-        expect(ifaces).to.have.length.above(0);
-    })
+  it('has properties', () => {
+    let info = JSON.parse(ifaces[0])
+    expect(info).to.have.property('name')
+    expect(info).to.have.property('status')
+    expect(info).to.have.property('mac')
+    expect(info).to.have.property('ip')
+    expect(info).to.have.property('netmask')
+    expect(info).to.have.property('broadcast')
+  })
 
-    it('has properties', () => {
-        let iface = JSON.parse(ifaces[0])
-        expect(iface).to.have.property('name')
-        expect(iface).to.have.property('status')
-        expect(iface).to.have.property('mac')
-        expect(iface).to.have.property('ip')
-        expect(iface).to.have.property('netmask')
-        expect(iface).to.have.property('broadcast')
-    })
+  it('has active interface', () => {
+    let active = helper.parseSVec(iface.get_interfaces(true))
+    for (a in active) {
+      let info = JSON.parse(active[a])
+      expect(info.status).equals('up')
+      expect(info.ip).to.not.equal('127.0.0.1')
+      expect(info.ip).to.not.equal('0.0.0.0')
+    }
+  })
 
-    it('has active interface', () => {
-        let active = helper.parseSVec(interface.get_interfaces(true))
-        for(a in active){
-            iface = JSON.parse(active[a])
-            expect(iface.status).equals('up')
-            expect(iface.ip).to.not.equal('127.0.0.1')
-            expect(iface.ip).to.not.equal('0.0.0.0')
-        }
-    })
-
-    it('has localhost', () => {
-        let local = helper.parseSVec(interface.get_localhosts())
-        for(a in local){
-            iface = JSON.parse(local[a])
-            expect(iface.status).equals('up')
-            expect(iface.ip).to.oneOf(['127.0.0.1','0.0.0.0'])
-        }
-    })
+  it('has localhost', () => {
+    let local = helper.parseSVec(iface.get_localhosts())
+    for (a in local) {
+      iface = JSON.parse(local[a])
+      expect(iface.status).equals('up')
+      expect(iface.ip).to.oneOf(['127.0.0.1', '0.0.0.0'])
+    }
+  })
 })
 
-describe('sniffer',() => {
-
-    const sniffer = new TransportSniffer()
-    num_packets = 0
-    killed = false
-    it('can sniff', () => {
-      sniffer.on('packet', (d) => {
-        if(num_packets <2){
-          console.log(d);
-          num_packets += 1
-        }
-        else{
-          if(!killed){
-            killed =true
-            console.log('Killing');
-            sniffer.kill()
-          }
-
-        }
-      })
+describe('sniffer', () => {
+  const sniffer = new TransportSniffer(10)
+  it('correct packet size', () => {
+    sniffer.on('packet', (d) => {
+      expect(Object.keys(d).length).equals(4)
     })
+  })
 })
